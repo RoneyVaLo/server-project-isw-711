ee8f922f09d945b29f6433a05effab44f80f10b5
Roney Valdelomar <rvaldelomarlopez@gmail.com>
2023-07-07 02:59:40 -0600

    creation of user and session models for the DB

---
+++ models/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const user = new Schema({
+    first_name: { type: String },
+    last_name: { type: String },
+    age: { type: Number },
+    role: { type: String },
+    user_name: { type: String },
+    password: { type: String },
+    profile_image: { type: String }
+});
+
+module.exports = mongoose.model('Users', user);
